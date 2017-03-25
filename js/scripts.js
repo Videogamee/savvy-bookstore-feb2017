16fdd5f543ba6192641a52138c3f501ef865ef4c
TravisHelton <traviskhelton@gmail.com>
2017-03-25 13:16:45 -0400

    scripts

--- js/scripts.js
+++ js/scripts.js
@@ -60,7 +60,15 @@ var generateItem = function generateItem( product ){
     $( "#book" + product.id + " span" ).text( product.id );
 };
 
-generateItem( book1 );
-generateItem( book2 );
-generateItem( book3 );
-generateItem( book4 );
+var books=[
+  book1,
+  book2,
+  book3,
+  book4
+];
+
+var i = 0;
+while(i < books.length){
+  generateItem(books[i]);
+  i++;
+}
